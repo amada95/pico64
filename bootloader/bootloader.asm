5638e0c0a0d09f5ccedfb120df2a4327552f9428
rosewritescode <10301172-rosewritescode@users.noreply.gitlab.com>
2022-01-19 21:06:22 -0600

    Add bootloader linker script and expand bootloader stack to 16KB

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,25 +1,27 @@
-; begin first sector (loaded and run by BIOS on startup)
+; bootloader.asm ;
 
+section .boot
 bits 16					; set assembler output to 16-bits (to run in real mode)
-org 0x7c00				; set assembler output to start at the end of the MBR
-
+global boot
 boot:
 	mov ax, 0x2401
-	int 0x15			; enable A20 bit (to enable accessing more than 1MB of memory)
+	int 0x15			; interrupt and enable A20 bit (to enable accessing more than 1MB of memory)
+	
 	mov ax, 0x3
-	int 0x10			; set vga text mode to 3
+	int 0x10			; interrupt and set vga text mode to 3
 
 	mov [disk], dl			; move dl (BIOS populates with disk number) into disk index
 
 	mov ah, 0x2			; read sectors
-	mov al, 1			; sectors to read
+	mov al, 6			; sectors to read
 	mov ch, 0			; cylinder index
 	mov dh, 0			; head index
 	mov cl, 2			; sector index
 	mov dl, [disk]			; disk index
 	mov bx, copy_target		; copy target pointer
-	int 0x13
-	cli				; disable interrupts during read
+	int 0x13			; 0x13 = Disk Services interrupt
+	cli				; clear interrupts
+	
 	lgdt [gdt_pointer]		; load global descriptor table (gdt)
 	mov eax, cr0
 	or eax, 0x1			; set protected mode (32-bit mode) bit on special register cr0
@@ -51,7 +53,6 @@ gdt_data:
 	db 11001111b
 	db 0x0
 gdt_end:
-
 gdt_pointer:
 	dw gdt_end - gdt_start		; gdt size
 	dd gdt_start			; pointer to gdt structure
@@ -60,13 +61,18 @@ disk:
 CODE_SEGMENT equ gdt_code - gdt_start	; pointer to gdt code segment
 DATA_SEGMENT equ gdt_data - gdt_start	; pointer to gdt data segment
 
-times 510 - ($-$$) db 0			; pad remaining 510 bytes with zeroes
+times 510 - ($-$$) db 0			; pad remaining 510 bytes of first sector with zeroes
 dw 0xaa55				; append bootable signature
 
-; end first sector
-
-
 copy_target:
-bits 32
-;; TODO: handoff execution to the kernel entry point
-times 1024 - ($-$$) db 0		; pad remaining 1024 bytes (2 sectors) with zeroes
+bits 32					; set assembler output to 32-bits (to run in protected mode)
+
+bootloader:	;; TODO: hand off execution to kernel entry point
+	nop
+	jmp bootloader
+
+section .bss
+align 4
+kernel_stack_bottom: equ $
+	resb 16384			; reserve 16KB for kernel stack
+kernel_stack_top:
